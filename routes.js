74c8332040ce09449abfe8857175b213a6ad1edd
ShivamMathwad <53596506+ShivamMathwad@users.noreply.github.com>
2020-06-01 23:15:49 +0530

    Nothing

--- routes.js
+++ routes.js
@@ -13,8 +13,8 @@ router.get("/", function(req,res){
 //Handles signup logic
 router.post("/signup", function(req,res){
     console.log("inside signup");
-    let user = req.body.username;
-    console.log("Got username = "+user);
+    let user2 = req.body.username;
+    console.log("Got username = "+user2);
     
     console.log(req.body);
     var user = {
