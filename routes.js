d27b363f3138a26abcea2885ee25af8eb928389d
ShivamMathwad <53596506+ShivamMathwad@users.noreply.github.com>
2020-07-08 00:50:03 +0530

    Nothing

--- routes.js
+++ routes.js
@@ -372,7 +372,7 @@ router.post("/getNAresult", function(req,res){
             status.status = "Test Not Given";
             res.send(status);
         } else {
-            if(Object.keys(foundEntry.numerical).length === 0 ){
+            if(foundEntry.numerical == null){
                 //Means user has not given the test
                 status.status = "Test Not Given";
                 console.log("Inside this");
