ad8f04d268d2570b33d402923c386b49543ad413
ShivamMathwad <53596506+ShivamMathwad@users.noreply.github.com>
2020-07-24 18:18:36 +0530

    Nothing

---
+++ Routes/grad_routes.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+//Schema Setup
+var User              = require("../Models/users.js");
+var GradQuestions     = require("../Models/grad_questions.js");
+
+
+//Routes     
+
+
+module.exports = router;
